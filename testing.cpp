6c8f32eb2dcaea540fb3f22ce2032b4a28bca785
hlebbatonov <glebplatonov08@gmail.com>
2026-08-24 16:59:42 +0300

    Smart fragmentation (2files)
    testing.cpp and main.cpp

--- testing.cpp
+++ testing.cpp
@@ -7,3 +7,127 @@ int main()
     RunAllTests();
 
 }
+
+void RunAllTests()
+{
+    TestKeys all_tests[10] =
+    {
+        {
+            .a = 0,
+            .b = 1,
+            .c = -1,
+            .number_of_roots_ref = ONE,
+            .x1_ref = 1,
+            .x2_ref = 0,
+        },
+
+        {
+            .a = 0,
+            .b = 0,
+            .c = -1,
+            .number_of_roots_ref = ZERO,
+            .x1_ref = 0,
+            .x2_ref = 0,
+        },
+
+        {
+            .a = 10,
+            .b = 0,
+            .c = -10,
+            .number_of_roots_ref = TWO,
+            .x1_ref = 1,
+            .x2_ref = -1,
+        },
+
+        {
+            .a = 0,
+            .b = 0,
+            .c = 0,
+            .number_of_roots_ref = INF,
+            .x1_ref = 0,
+            .x2_ref = 0,
+        },
+
+        {
+            .a = 1,
+            .b = -8,
+            .c = 15,
+            .number_of_roots_ref = TWO,
+            .x1_ref = 5,
+            .x2_ref = 3,
+        },
+
+        {
+            .a = 0,
+            .b = 10,
+            .c = -1,
+            .number_of_roots_ref = ONE,
+            .x1_ref = 0.1,
+            .x2_ref = 0,
+        },
+
+        {
+            .a = 5,
+            .b = 8,
+            .c = -17,
+            .number_of_roots_ref = TWO,
+            .x1_ref = 1.20997,
+            .x2_ref = -2.80997,
+        },
+
+        {
+            .a = 0,
+            .b = -2,
+            .c = -1,
+            .number_of_roots_ref = ONE,
+            .x1_ref = -0.5,
+            .x2_ref = 0,
+        },
+
+        {
+            .a = 2,
+            .b = -9,
+            .c = -10,
+            .number_of_roots_ref = TWO,
+            .x1_ref = 5.422144,
+            .x2_ref = -0.922144,
+        },
+
+        {
+            .a = 0,
+            .b = 1,
+            .c = 0,
+            .number_of_roots_ref = ONE,
+            .x1_ref = 0,
+            .x2_ref = 0,
+        }
+    };
+    for (int i = 0; i < 10; i ++)
+    {
+        RunOneTest(all_tests[i]);
+    }
+
+
+}
+
+
+int RunOneTest(TestKeys test)
+{
+    double x1 = 0, x2 = 0;
+    double coeffs[3] = {test.a, test.b, test.c};
+    Roots number_of_roots = SolveEquation(coeffs, &x1, &x2);
+
+    if (number_of_roots != test.number_of_roots_ref || !IsEqual(x1, test.x1_ref) || !IsEqual(x2, test.x2_ref))
+    {
+        printf("Test FAILED: a = %lg, b = %lg, c = %lg\n"
+               "REFERENCE: %d roots, x1_ref = %lg, x2_ref = %lg\n"
+               "GOT: %d roots, x1 = %lg, x2 = %lg\n",
+               test.a, test.b, test.c, test.number_of_roots_ref, test.x1_ref, test.x2_ref, number_of_roots, x1, x2);
+    }
+    else
+    {
+        printf("Test OK\n");
+    }
+
+    return 0;
+}
